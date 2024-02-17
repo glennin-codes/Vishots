e5d28af8d9d37a373feccc080d9944e068195692
glennin-codes <glen.nyandiri@students.kyu.ac.ke>
2024-02-17 07:46:04 +0300

    changed data structures

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -10,8 +10,18 @@ export interface VideoModel {
   }
   
 const  VideoDatas:VideoModel[]= [
+  {
+    id: 1,
+    channelName: 'meow',
+    uri: 'https://v.pinimg.com/videos/mc/720p/11/05/2c/11052c35282355459147eabe31cf3c75.mp4',
+    caption: 'Doggies eating candy #cute #puppy',
+    musicName: 'Song #2',
+    likes: 2411,
+    comments: 1222,
+    avatarUri: 'https://wallpaperaccess.com/thumb/266770.jpg',
+  },
     {
-      id: 1,
+      id: 2,
       channelName: 'best-music',
       uri: 'https://mediaglens.s3.amazonaws.com/94d0479c8d07/videos/75260fdc-9a11-4cdb-b5cf-aca0ecde3a0f.mp4',
       caption: 'Cute dog shaking hands #cute #puppy',
@@ -20,16 +30,7 @@ const  VideoDatas:VideoModel[]= [
       comments: 2841,
       avatarUri: 'https://wallpaperaccess.com/full/1669289.jpg',
     },
-    {
-      id: 2,
-      channelName: 'cutedog',
-      uri: 'https://mediaglens.s3.amazonaws.com/94d0479c8d07/videos/75260fdc-9a11-4cdb-b5cf-aca0ecde3a0f.mp4',
-      caption: 'Some cool music you may wanna view to #music #cool #vibes',
-      musicName: 'Song #1',
-      likes: 4321,
-      comments: 2841,
-      avatarUri: 'https://wallpaperaccess.com/full/1669289.jpg',
-    },
+    
     {
       id: 3,
       channelName: 'cutedog',
@@ -40,18 +41,9 @@ const  VideoDatas:VideoModel[]= [
       comments: 2841,
       avatarUri: 'https://wallpaperaccess.com/full/1669289.jpg',
     },
+  
     {
       id: 4,
-      channelName: 'meow',
-      uri: 'https://v.pinimg.com/videos/mc/720p/11/05/2c/11052c35282355459147eabe31cf3c75.mp4',
-      caption: 'Doggies eating candy #cute #puppy',
-      musicName: 'Song #2',
-      likes: 2411,
-      comments: 1222,
-      avatarUri: 'https://wallpaperaccess.com/thumb/266770.jpg',
-    },
-    {
-      id: 5,
       channelName: 'yummy',
       uri: 'https://v.pinimg.com/videos/mc/720p/c9/22/d8/c922d8391146cc2fdbeb367e8da0d61f.mp4',
       caption: 'Brown little puppy #cute #puppy',
@@ -60,5 +52,17 @@ const  VideoDatas:VideoModel[]= [
       comments: 801,
       avatarUri: 'https://wallpaperaccess.com/thumb/384178.jpg',
     },
+    {
+      id: 5,
+    channelName: 'cutedog',
+    uri: 'https://mediaglens.s3.amazonaws.com/94d0479c8d07/videos/75260fdc-9a11-4cdb-b5cf-aca0ecde3a0f.mp4',
+    caption: 'Some cool music you may wanna view to #music #cool #vibes',
+    musicName: 'Song #1',
+    likes: 4321,
+    comments: 2841,
+    avatarUri: 'https://wallpaperaccess.com/full/1669289.jpg',
+  },
+   
+    
   ];
   export default VideoDatas;
